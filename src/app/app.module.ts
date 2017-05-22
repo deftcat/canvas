40bdf9ed6d0e1583a7e8e21f614e8fd7515e5750
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-22 17:34:09 +0800

    add canvas and draw a circle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CirclesComponent } from './circles/circles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CirclesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/circles/circles.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-circles',
+  templateUrl: './circles.component.html',
+  styleUrls: ['./circles.component.css']
+})
+export class CirclesComponent implements OnInit {
+  width: string = window.innerWidth.toString() + 'px';
+  x: number;
+  @ViewChild('myCanvas') canvasRef: ElementRef;
+  context: CanvasRenderingContext2D;
+  constructor() { }
+
+  ngOnInit() {
+    // let ctx = this.context;
+    // // the following is woking
+    // let ctx :CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext("2d");
+    // ctx.fillRect(100, 100, 100, 100);
+
+
+    //use draw is better
+    this.context = this.canvasRef.nativeElement.getContext("2d");
+    this.x = 0;
+    this.draw();
+  }
+
+  draw() {
+    console.log('hit draw');
+    requestAnimationFrame(() => {
+      this.draw();
+      // console.log('here');
+    });
+    let ctx = this.context;
+    // for (this.x = 0; this.x+= 0.1; this.x < window.innerWidth){
+    ctx.beginPath();
+    ctx.arc(this.x, 50, 4, 0, Math.PI * 2, false);
+    // ctx.str
+    ctx.strokeStyle = 'pink';
+    ctx.stroke();
+    // }
+
+    // this.x += 0.1;
+
+    // ctx.fillRect
+    // ctx.clearRect(0, 0, 400, 400);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(0, 0, 50, 60);
+  }
+
+}
