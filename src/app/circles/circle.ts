787fd31e7e1d2f7cd2aea6a57c9ad2834a945782
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-23 11:50:20 +0800

    extract circle to a class for futeher object

---
+++ src/app/circles/circle.ts
@@ -0,0 +1,28 @@
+export class Circle {
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+    radious: number;
+    ctx: CanvasRenderingContext2D;
+    constructor(x: number, y: number, dx: number, dy: number, radious: number = 40) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radious = radious;
+    }
+    draw(ctx) {
+        this.ctx = ctx;
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
+        this.ctx.strokeStyle = 'purple';
+        this.ctx.stroke();
+    }
+    update(width: number, height: number) {
+        this.dx = ((this.x > width - this.radious) || (this.x < 0 + this.radious)) ? -this.dx : this.dx;
+        this.dy = ((this.y > height - this.radious) || (this.y < 0 + this.radious)) ? -this.dy : this.dy;
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
--- src/app/circles/circles.component.ts
+++ src/app/circles/circles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Circle } from './circle';
 
 @Component({
   selector: 'app-circles',
@@ -13,6 +14,7 @@ export class CirclesComponent implements OnInit {
   dx: number;
   dy: number;
   radious: number;
+  circle: Circle;
   @ViewChild('myCanvas') canvasRef: ElementRef;
   // context: CanvasRenderingContext2D;
   ctx: CanvasRenderingContext2D;
@@ -33,24 +35,31 @@ export class CirclesComponent implements OnInit {
     this.radious = 40;
     this.x = Math.random() * this.width;//0 + this.radious;
     this.y = Math.random() * this.height;//0 + this.radious;
-    this.draw();
+    this.circle = new Circle(this.x, this.y, this.dx, this.dy, this.radious);
+    this.animate();
+
   }
 
-  draw() {
+  animate() {
     console.log('hit draw');
+
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.circle.draw(this.ctx);
+    this.circle.update(this.width, this.height);
     requestAnimationFrame(() => {
-      this.draw();
+      this.animate();
       // console.log('here');
     });
-    this.ctx.clearRect(0, 0, this.width, this.height);
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
-    this.ctx.strokeStyle = 'purple';
-    this.ctx.stroke();
-    this.dx = ((this.x > this.width - this.radious) || (this.x < 0 + this.radious)) ? -this.dx : this.dx;
-    this.dy = ((this.y > this.height - this.radious) || (this.y < 0 + this.radious)) ? -this.dy : this.dy;
-    this.x += this.dx;
-    this.y += this.dy;
+    // this.ctx.clearRect(0, 0, this.width, this.height);
+    // this.ctx.beginPath();
+    // this.ctx.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
+    // this.ctx.strokeStyle = 'purple';
+    // this.ctx.stroke();
+
+    // this.dx = ((this.x > this.width - this.radious) || (this.x < 0 + this.radious)) ? -this.dx : this.dx;
+    // this.dy = ((this.y > this.height - this.radious) || (this.y < 0 + this.radious)) ? -this.dy : this.dy;
+    // this.x += this.dx;
+    // this.y += this.dy;
   }
 
 }
