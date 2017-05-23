6f4e44f3ff08dd02f732dcacf7899caa935fa2ed
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-23 15:53:52 +0800

    use array to generate circles

--- src/app/circles/circles.component.ts
+++ src/app/circles/circles.component.ts
@@ -15,7 +15,7 @@ export class CirclesComponent implements OnInit {
   dy: number;
   radious: number;
   // circle: Circle;
-  circles : Circle[]=[];
+  circles: Circle[] = [];
   @ViewChild('myCanvas') canvasRef: ElementRef;
   // context: CanvasRenderingContext2D;
   ctx: CanvasRenderingContext2D;
@@ -27,52 +27,36 @@ export class CirclesComponent implements OnInit {
     // let ctx :CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext("2d");
     // ctx.fillRect(100, 100, 100, 100);
 
-
     //use draw is better
     // this.context = this.canvasRef.nativeElement.getContext("2d");
     this.ctx = this.canvasRef.nativeElement.getContext("2d");
-    for (let index : number = 0; index < 100; index++) {
+    for (let index: number = 0; index < 100; index++) {
 
       this.dx = (Math.random() - 0.5) * 10;
       this.dy = (Math.random() - 0.5) * 10;
       this.radious = 40;
-      this.x = Math.random() * this.width;//0 + this.radious;
-      this.y = Math.random() * this.height;//0 + this.radious;
+      this.x = Math.random() * (this.width - this.radious * 2) + this.radious;//0 + this.radious;
+      this.y = Math.random() * (this.height - this.radious * 2) + this.radious;//0 + this.radious;
       // debugger;
       // this.circle = new Circle(this.x, this.y, this.dx, this.dy, this.radious);
       this.circles.push(new Circle(this.x, this.y, this.dx, this.dy, this.radious));
-      // console.log(this.circle);
-      
-      // this.circle.push(new Circle(this.x, this.y, this.dx, this.dy, this.radious));
-
     }
-    console.log(this.circles);
-    
     // this.circle = new Circle(this.x, this.y, this.dx, this.dy, this.radious);
-    // this.animate();
+    this.animate();
 
   }
 
   animate() {
     console.log('hit draw');
-
     this.ctx.clearRect(0, 0, this.width, this.height);
-    this.circle.draw(this.ctx);
-    this.circle.update(this.width, this.height);
     requestAnimationFrame(() => {
       this.animate();
-      // console.log('here');
     });
-    // this.ctx.clearRect(0, 0, this.width, this.height);
-    // this.ctx.beginPath();
-    // this.ctx.arc(this.x, this.y, this.radious, 0, Math.PI * 2, false);
-    // this.ctx.strokeStyle = 'purple';
-    // this.ctx.stroke();
+    for (let index = 0; index < this.circles.length; index++) {
+      this.circles[index].draw(this.ctx);
+      this.circles[index].update(this.width, this.height);
 
-    // this.dx = ((this.x > this.width - this.radious) || (this.x < 0 + this.radious)) ? -this.dx : this.dx;
-    // this.dy = ((this.y > this.height - this.radious) || (this.y < 0 + this.radious)) ? -this.dy : this.dy;
-    // this.x += this.dx;
-    // this.y += this.dy;
+    }
   }
 
 }
