b9dd384da61c21e1b47df58900a05a21301ae6be
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-06-04 08:19:50 +0800

    Add router-link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CirclesComponent } from './circles/circles.component';
+import { GravityComponent } from './gravity/gravity.component';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routers: Routes = [{
+  path: '',
+  redirectTo: '/gravity',
+  pathMatch: 'full'
+},{
+  path: 'circles',
+  component: CirclesComponent
+}, {
+  path: 'gravity',
+  component: GravityComponent
+}, ];
 @NgModule({
   declarations: [
     AppComponent,
-    CirclesComponent
+    CirclesComponent,
+    GravityComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routers),
   ],
   providers: [],
   bootstrap: [AppComponent]
